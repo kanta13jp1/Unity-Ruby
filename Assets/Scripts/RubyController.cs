38eca446ebf155a1bd813dcc61b0c722bcc199a8
kanta13jp1 <kanta13jp@gmail.com>
2022-02-14 21:52:17 +0900

    commit

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -15,6 +15,6 @@ public class NewBehaviourScript : MonoBehaviour
     {
         Vector2 position = transform.position;
         position.x = position.x + 0.1f;
-        transform.position = position;        
+        transform.position = position;
     }
 }
