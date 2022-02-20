ed578737a57305a78fd01c2e03d66d33c4598a09
kanta13jp1 <kanta13jp@gmail.com>
2022-02-20 17:37:25 +0900

    Enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -45,4 +45,14 @@ public class EnemyController : MonoBehaviour
         
         rigidbody2d.MovePosition(position);
     }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
 }
