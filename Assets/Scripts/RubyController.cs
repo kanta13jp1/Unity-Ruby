85bc8be3dad67855e28afe2caaa647ac48c9889c
kanta13jp1 <kanta13jp@gmail.com>
2022-02-22 21:36:15 +0900

    Projectile

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -9,6 +9,8 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
 
+    public GameObject projectilePrefab;
+
     public int health { get { return currentHealth; }}
     int currentHealth;
     
@@ -55,6 +57,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
     }
     
     void FixedUpdate()
@@ -80,5 +87,15 @@ public class RubyController : MonoBehaviour
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
