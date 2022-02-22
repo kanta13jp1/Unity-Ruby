7f93da49bb5a1cdc535757045965d72871495eff
kanta13jp1 <kanta13jp@gmail.com>
2022-02-23 05:22:49 +0900

    destroy projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -15,7 +15,10 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }        
     }
 
     public void Launch(Vector2 direction, float force)
