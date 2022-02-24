154269ddc13854b42ae1ad925e2860eeb471d85f
kanta13jp1 <kanta13jp@gmail.com>
2022-02-24 09:23:31 +0900

    Particle System

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -58,6 +58,36 @@ public class RubyController : MonoBehaviour
                 isInvincible = false;
         }
 
+        if (Input.GetKeyDown ("joystick button 0")) {
+            Debug.Log ("button0");
+        }
+        if (Input.GetKeyDown ("joystick button 1")) {
+            Debug.Log ("button1");
+        }
+        if (Input.GetKeyDown ("joystick button 2")) {
+            Debug.Log ("button2");
+        }
+        if (Input.GetKeyDown ("joystick button 3")) {
+            Debug.Log ("button3");
+        }
+        if (Input.GetKeyDown ("joystick button 4")) {
+            Debug.Log ("button4");
+        }
+        if (Input.GetKeyDown ("joystick button 5")) {
+            Debug.Log ("button5");
+        }
+        if (Input.GetKeyDown ("joystick button 6")) {
+            Debug.Log ("button6");
+        }
+        if (Input.GetKeyDown ("joystick button 7")) {
+            Debug.Log ("button7");
+        }
+        if (Input.GetKeyDown ("joystick button 8")) {
+            Debug.Log ("button8");
+        }
+        if (Input.GetKeyDown ("joystick button 9")) {
+            Debug.Log ("button9");
+        }
         if(Input.GetKeyDown(KeyCode.C))
         {
             Launch();
